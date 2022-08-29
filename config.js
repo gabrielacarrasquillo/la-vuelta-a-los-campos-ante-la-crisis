9e5756b9a6a864b13fb0c4d69c25c3500099188f
Carrasquillo-Piñeiro <107572177+gabrielacarrasquillo@users.noreply.github.com>
2022-08-29 16:44:40 -0400

    update

--- config.js
+++ config.js
@@ -12,54 +12,54 @@ var config = {
     // title: 'The Title Text of this Story',
     // subtitle: 'A descriptive and interesting subtitle to draw in the reader',
     // byline: 'By a Digital Storyteller',
-    footer: 'Data: Agroecological organizations. <br> Created using <a href="https://github.com/mapbox/storytelling" target="_blank">Mapbox Storytelling</a> template.',
+    footer: 'Note: Por razones de seguridad, los puntos no representan la localización exacta de las fincas. Data: Agroecological organizations. <br> Created using <a href="https://github.com/mapbox/storytelling" target="_blank">Mapbox Storytelling</a> template.',
     chapters: [
-        {
-            id: 'entrada',
-            alignment: 'left',
-            hidden: false,
-            title: 'Coralys Márquez',
-            image: './path/to/image/source.png',
-            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-            location: {
-                center: [-66.85163, 18.29060],
-                zoom: 17,
-                pitch: 20.00,
-                bearing: 164.00
-            },
-            mapAnimation: 'flyTo',
-            rotateAnimation: false,
-            callback: '',
-            onChapterEnter: [
+        // {
+        //     id: 'entrada',
+        //     alignment: 'left',
+        //     hidden: false,
+        //     title: 'Coralys Márquez',
+        //     image: './path/to/image/source.png',
+        //     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        //     location: {
+        //         center: [-66.85163, 18.29060],
+        //         zoom: 17,
+        //         pitch: 20.00,
+        //         bearing: 164.00
+        //     },
+        //     mapAnimation: 'flyTo',
+        //     rotateAnimation: false,
+        //     callback: '',
+        //     onChapterEnter: [
 
-            ],
-            onChapterExit: [
-                { layer: 'region_metro',
-                opacity: 0,
-                duration: 1000
-                },
-                { layer: 'region-north',
-                opacity: 0,
-                duration: 1000
-                },
-                { layer: 'region_south',
-                opacity: 0,
-                duration: 1000
-                },
-                { layer: 'region_oeste',
-                opacity: 0,
-                duration: 1000
-                },
-                { layer: 'region_centro',
-                opacity: 0,
-                duration: 1000
-                },
-                { layer: 'region_east',
-                opacity: 0,
-                duration: 1000
-                },
-            ]
-        },
+        //     ],
+        //     onChapterExit: [
+        //         { layer: 'region_metro',
+        //         opacity: 0,
+        //         duration: 1000
+        //         },
+        //         { layer: 'region-north',
+        //         opacity: 0,
+        //         duration: 1000
+        //         },
+        //         { layer: 'region_south',
+        //         opacity: 0,
+        //         duration: 1000
+        //         },
+        //         { layer: 'region_oeste',
+        //         opacity: 0,
+        //         duration: 1000
+        //         },
+        //         { layer: 'region_centro',
+        //         opacity: 0,
+        //         duration: 1000
+        //         },
+        //         { layer: 'region_east',
+        //         opacity: 0,
+        //         duration: 1000
+        //         },
+        //     ]
+        // },
         // {
         //     id: 'parte-2',
         //     alignment: 'left',
@@ -83,14 +83,14 @@ var config = {
             id: 'parte-3',
             alignment: 'left',
             hidden: false,
-            title: 'Entire archipelago with all farms',
+            title: '',
             image: 'mon.png',
-            description: 'Copy these sections to add to your story.',
+            description: 'Una mirada cercana a proyectos agrícolas que se identifican como agroecológicos, se encontraron al menos 93 iniciativas campesinas, y 80 de estas indican, en sus redes sociales, el pueblo en dónde se ubican. En su mayoría se encuentran en municipios en zonas montañosas. ',
             location: {
                 center: [-66.51191, 18.38216],
                 zoom: 8.35,
-                pitch: 20.00,
-                bearing: 6.00,
+                pitch: 15.00,
+                bearing: 3.00,
                 // flyTo additional controls-
                 // These options control the flight curve, making it move
                 // slowly and zoom out almost completely before starting
@@ -104,10 +104,18 @@ var config = {
             onChapterEnter: [
                 { layer: 'puntos-fincas',
                 opacity: 1,
-                duration: 20000
+                duration: 2000
+                },
+                { layer: 'puntos-edu',
+                opacity: 0,
+                duration: 2000
                 },
             ],
             onChapterExit: [
+                { layer: 'puntos-edu',
+                opacity: 0,
+                duration: 500
+                },
                 { layer: 'region_metro',
                 opacity: 0,
                 duration: 1000
@@ -137,9 +145,9 @@ var config = {
             id: 'parte-4',
             alignment: 'right',
             hidden: false,
-            title: 'All regions appear but region_central',
-            image: '',
-            description: 'Copy these sections to add to your story.',
+            title: '',
+            image: 'centro.jpg',
+            description: '30 fincas agroecológicas especificaron estar ubicadas en pueblos de la región centro.',
             location: {
                 center: [-66.30314, 18.37376],
                 zoom: 8.44,
@@ -177,7 +185,10 @@ var config = {
                 },
             ],
             onChapterExit: [
-
+                { layer: 'puntos-edu',
+                opacity: 0,
+                duration: 500
+                },
 
             ]
         },
@@ -185,9 +196,9 @@ var config = {
             id: 'parte-5',
             alignment: 'right',
             hidden: false,
-            title: 'All regions appear but region_oeste',
-            image: '',
-            description: 'Copy these sections to add to your story.',
+            title: '',
+            image: 'oeste.jpg',
+            description: 'Por su parte, 19 indican estar ubicadas en pueblos de la región oeste del archipiélago.',
             location: {
                 center: [-66.30314, 18.37376],
                 zoom: 8.44,
@@ -222,8 +233,16 @@ var config = {
                 opacity: 1,
                 duration: 1000
                 },
+                { layer: 'puntos-edu',
+                opacity: 0,
+                duration: 500
+                },
             ],
             onChapterExit: [
+                { layer: 'puntos-edu',
+                opacity: 0,
+                duration: 500
+                },
                 { layer: 'region_metro',
                 opacity: 1,
                 duration: 2000
@@ -255,9 +274,9 @@ var config = {
             id: 'parte-6',
             alignment: 'right',
             hidden: false,
-            title: 'Salen todas las regiones y agunas bolitas se pintan de verde. FALTA duplicar layer en mapbox y escoger la cantidad de bolitas y crear anotaciones en el mapa con el número de bolitas que corresponde a cada categoría.',
+            title: '',
             image: 'edu.jpg',
-            description: 'Graph: Gabriela Alexandra Carrasquillo Piñeiro',
+            description: 'En sus descripciones y mediante el contenido que publican en sus plataformas, 41 iniciativas se identificaron como fincas familiares, 15 como proyectos comunitarios, 10 solo como fincas, tres cooperativas y una organización. Bajo "others" se encuentran también unos nueve proyectos que no especifican.',
             location: {
                 center: [-66.04154, 18.38091],
                 zoom: 8.05,
@@ -269,29 +288,33 @@ var config = {
             rotateAnimation: false,
             callback: '',
             onChapterEnter: [
+                { layer: 'puntos-edu',
+                opacity: 1,
+                duration: 2000
+                },
                 { layer: 'region_metro',
                 opacity: 0,
-                duration: 2000
+                duration: 500
                 },
                 { layer: 'region-north',
                 opacity: 0,
-                duration: 2000
+                duration: 500
                 },
                 { layer: 'region_south',
                 opacity: 0,
-                duration: 2000
+                duration: 500
                 },
                 { layer: 'region_oeste',
                 opacity: 0,
-                duration: 2000
+                duration: 500
                 },
                 { layer: 'region_centro',
                 opacity: 0,
-                duration: 2000
+                duration: 500
                 },
                 { layer: 'region_east',
                 opacity: 0,
-                duration: 2000
+                duration: 500
                 },
             ],
             onChapterExit: [
